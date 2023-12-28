ea8e5d156bed50208bed5333bf6fe99f54f8efc5
Sooyeon Noh <tndus1739@gmail.com>
2023-12-28 21:01:40 +0900

    231228

--- Oracle Script/HR12/HR12 계정으로 접속.sql
+++ Oracle Script/HR12/HR12 계정으로 접속.sql
@@ -10,13 +10,36 @@ create table board (
      cnt number (5) default 0 null
  );
  
+ select * from board ;
+ 
+ update board 
+ set title = '제목 수정됨' , write = '이순신' , content = '내용 수정됨' 
+ where seq = 4;
+ commit ;
+ rollback;
+ delete board where seq = 8;
+ 
+ update board 
+ set cnt = cnt + 1 
+ where seq = 8 ;
+ 
+ 
+ desc board ;
+ select * from board;
+ select * from board order by seq desc;
+ 
  create table users (
     id varchar2(8) not null primary key,
     password varchar2 (8) null,
     name varchar2(20) null ,
     role varchar2(5)
 );
-
+ desc users;
+ select * from users 
+ where id = 'user'  
+ and password = '1234';
+ 
+ 
 create table products (
     p_code number(5) not null primary key ,
     p_name varchar2(200) null ,
---
+++ Oracle Script/TEST231228/TEST.sql
@@ -0,0 +1,39 @@
+
+create table Member(
+    id varchar2(255) not null primary key,
+    password varchar2(255) not null,
+    phone varchar2 (255) not null,
+    email varchar2(255) null,
+    regdate date default sysdate,
+    addr varchar2(255),
+    role varchar2(255) default 'user'
+);
+
+create table product (
+    id number(5)  not null primary key,  
+    name varchar2(255) not null,              
+    price number(8,2) not null,                    
+    content varchar2(2000),                        
+    regdate date default sysdate              
+    );
+
+create table m_board (
+    id number(5) not null primary key,
+    m_title varchar2 (255) not null ,
+    m_write varchar2(255) not null,
+    m_cont varchar2 (255) not null,
+    regdate date default sysdate,
+    cnt number(5) default 0
+    );
+
+select * from member;
+select * from product;
+select * from m_board;
+
+insert into member
+values ( 'admin' , '1234' , '010-0000-0000' , 'aaa@aaa' , default , 'ooo' , default ) ;
+insert into member
+values ( 'user' , '1234' , '010-1111-1111' , 'aaa@aaa' , default , 'ooo' , default ) ;
+
+commit;
+
